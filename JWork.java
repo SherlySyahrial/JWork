8f1bbfabbcdf3976cee8c6dc1eec13619feba05f
SherlySyahrial <sherly82@ui.ac.id>
2021-03-25 18:13:04 +0700

    Modul 3

--- JWork.java
+++ JWork.java
@@ -9,6 +9,7 @@ public class Jwork{
         Location location1 = new Location("DKI Jakarta", "Jakarta", "Ibu Kota");
         Recruiter recruite1 = new Recruiter(1, "Sherly", "sherly82@ui.ac.id", "0895637478937", location1);
         Job job1 = new Job(1, 10000000, "Software Engineer", JobCategory.UI, recruite1);
+        PaymentType pay1 = PaymentType.BankPayment;
         
         job1.printData();
     }
