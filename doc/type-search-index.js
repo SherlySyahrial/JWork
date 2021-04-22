d404cfbb460e5a22e599a80491e1b36788eaa169
SherlySyahrial <sherlysyahrial15@gmail.com>
2021-04-22 15:01:41 +0700

    precs modul 6

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"DatabaseJob"},{"p":"<Unnamed>","l":"Invoice"},{"p":"<Unnamed>","l":"Job"},{"p":"<Unnamed>","l":"Jobseeker"},{"p":"<Unnamed>","l":"Jwork"},{"p":"<Unnamed>","l":"Location"},{"p":"<Unnamed>","l":"Recruiter"}]
